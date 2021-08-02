455d674274efc9594ffae6c2946528392580e1d5
HoorayTritium <ryuutomi1110@gmail.com>
2021-08-02 15:38:51 +0900

    ignore

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        // Reference a variable that was defined in the .env file and make it available at Build Time
+        X_API_KEY: "9b30e206-4b28-4453-91f5-5d39d40d15a3",
+        SS_URL: "https://script.google.com/macros/s/AKfycbzAvyLZq8zl9ZOq_AkTyI7oY5AoLdXESHBegfYdUG9XSe7iEMEKU3WpwVbPPDbqXG7A/exec",
+        SS_POST_URL: "https://script.google.com/macros/s/AKfycbzJKJ6ZwLc6x0QEqYGNXfIA6UgmoNJ1kIH74qDrV-pu/dev"
+    },
+}
