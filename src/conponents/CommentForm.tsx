6ddbe825ff88572eb47948f3344e7fa97ecd1c75
HoorayTritium <ryuutomi1110@gmail.com>
2021-08-13 22:06:43 +0900

    actestv1

--- src/conponents/CommentForm.tsx
+++ src/conponents/CommentForm.tsx
@@ -2,6 +2,34 @@ import * as React from "react";
 
 function CommentForm(props) {
   const pageID = props.id;
+  const commentSended = () => {
+    const commentLoading = document.getElementById("commentLoading");
+    const commentName = document.getElementById("commentName");
+    const comment = document.getElementById("comment");
+    const isSend = commentName.value && comment.value;
+
+    commentLoading.style.display = "none";
+    if (isSend) {
+      commentName.value = "";
+      comment.value = "";
+      location.reload();
+    }
+  };
+
+  const commentSending = () => {
+    const commentLoading = document.getElementById("commentLoading");
+    const name = document.getElementById("commentName");
+    const comment = document.getElementById("comment");
+
+    if (!name.value || !comment.value) {
+      alert("名前とコメントを入力してください");
+      return false;
+    }
+    const date = document.getElementById("commentDate");
+    date.value = new Date().toISOString();
+    commentLoading.style.display = "flex";
+    return true;
+  };
 
   return (
     <>
@@ -10,6 +38,7 @@ function CommentForm(props) {
         method="POST"
         action={process.env.SS_POST_URL}
         target="response"
+        onSubmit={commentSending}
       >
         <div>名前</div>
         <input
@@ -37,9 +66,7 @@ function CommentForm(props) {
           <div className="commentCircle"></div>
         </div>
       </form>
-
-      <iframe name="response" id="response"></iframe>
-      <script id="hge" src="/comment.js"></script>
+      <iframe name="response" id="response" onLoad={commentSended}></iframe>
     </>
   );
 }
