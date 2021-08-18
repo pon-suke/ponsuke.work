2d8a919b0fba914a26a1a4c015f99ce1a02ccf8e
HoorayTritium <48468687+HoorayTritium@users.noreply.github.com>
2021-08-18 19:16:58 +0900

    access counter
    リレンダリング

--- public/onload.js
+++ public/onload.js
@@ -1,8 +1,22 @@
-(window.onload = () => {
-    const observer = new MutationObserver(function () {
-        // ページ切り替え時にサブスクボタンをレンダリング
-        window.twttr.widgets.load(document.getElementById("side"))
-        gapi.ytsubscribe.go(document.getElementById("side"))
-    })
-    observer.observe(document, { childList: true, subtree: true })
-})
+window.onload = () => {
+  const observer = new MutationObserver(function () {
+    // ページ切り替え時にサブスクボタンをレンダリング
+    try {
+      window.twttr.widgets.load(document.getElementById("side"));
+    } catch (error) {
+      console.error(error);
+    }
+    try {
+      gapi.ytsubscribe.go(document.getElementById("side"));
+    } catch (error) {
+      console.error(error);
+    }
+    try {
+      const fc2cnt = document.getElementById("fc2Counter");
+      if (!fc2cnt.childElementCount) _FC2COUNTER89495020_0.disp_js();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  observer.observe(document, { childList: true, subtree: true });
+};
