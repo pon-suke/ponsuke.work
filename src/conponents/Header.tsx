c171c5a07f7289c1d0a09eed20d36a15f6ea38db
HoorayTritium <ryuutomi1110@gmail.com>
2021-08-22 22:21:43 +0900

    v0.2
    adsense
    comment icon

--- src/conponents/Header.tsx
+++ src/conponents/Header.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
 import Link from "next/link";
+import AdSense from "react-adsense";
+import Script from "next/script";
 
 const Header: React.FC = () => (
-  <Link href="/p/1">
-    <div id="header">
-      <h1 className="noDecoration">pon&nbsp;suke人生リークBlog</h1>
-    </div>
-  </Link>
+  <>
+    <Script
+      data-ad-client="ca-pub-4852357139979343"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    ></Script>
+    <AdSense.Google client="ca-pub-4852357139979343" />
+    <Link href="/p/1">
+      <div id="header">
+        <h1 className="noDecoration">pon&nbsp;suke人生リークBlog</h1>
+      </div>
+    </Link>
+  </>
 );
 
 export default Header;
