8bbd09252dde01a4f0544e634a56492d13de29c8
HoorayTritium <ryuutomi1110@gmail.com>
2021-08-25 18:29:40 +0900

    v0.2
    旧記事の表示

--- public/onload.js
+++ public/onload.js
@@ -5,11 +5,11 @@ window.onload = () => {
     if (window.twttr)
       window.twttr.widgets.load(document.getElementById("side"));
 
-    if (gapi.ytsubscribe)
+    if (gapi && gapi.ytsubscribe)
       gapi.ytsubscribe.go(document.getElementById("side"));
 
     const fc2cnt = document.getElementById("fc2Counter");
-    if (!fc2cnt.childElementCount) _FC2COUNTER89495020_0.disp_js();
+    if (fc2cnt && !fc2cnt.childElementCount) _FC2COUNTER89495020_0.disp_js();
 
   });
   observer.observe(document, { childList: true, subtree: true });
