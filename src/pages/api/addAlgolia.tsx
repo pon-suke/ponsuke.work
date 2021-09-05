93cf8f6fe186600cb2c48860eac3251b01446546
HoorayTritium <ryuutomi1110@gmail.com>
2021-09-05 18:49:47 +0900

    Update addAlgolia.tsx

--- src/pages/api/addAlgolia.tsx
+++ src/pages/api/addAlgolia.tsx
@@ -4,45 +4,55 @@ export default (req, res) => {
   const query = req.query;
   const client = algoliasearch(`${process.env.ALGOLIA_APP_ID}`, `${process.env.ALGOLIA_ADMIN_KEY}`);
   const index = client.initIndex("ponsuke_work_old");
+  const body = req.body;
 
   let hits = [];
 
-  if (req.method === "POST") {
+  if (
+    req.method === "POST"
+    // && query.auth == "2J5vi1mT8sud"
+  ) {
     // Process a POST request
     console.log("post");
-    console.log(req.body);
+    console.log(JSON.stringify(req.body));
+    const newContents = body.contents.new.publishValue;
+
+    const objects = [
+      {
+        UpdatedDate: newContents.updatedAt,
+        title: newContents.title,
+        subTitle: newContents.subTitle,
+        main: newContents.main,
+        images: [],
+        movies: [],
+        id: newContents.id,
+      },
+    ];
+
+    // データがalgoliaに存在するか確認
+    index
+      .browseObjects({
+        query: "", // Empty query will match all records
+        filters: "id=" + body.id,
+        attributesToRetrieve: ["id"],
+        batch: (batch) => {
+          hits = hits.concat(batch);
+        },
+      })
+      .then(() => {
+        console.log(hits);
+        if (hits.length == 0) {
+          /******  algoliaに書き込む ******/
+          index
+            .saveObjects(objects, { autoGenerateObjectIDIfNotExist: true })
+            .then(({ objectIDs }) => {
+              console.log(objectIDs);
+            });
+        }
+      });
   } else {
     // Handle any other HTTP method
   }
-
-  // データがalgoliaに存在するか確認
-  index
-    .browseObjects({
-      query: "", // Empty query will match all records
-      filters: "id=42",
-      attributesToRetrieve: ["id"],
-      batch: (batch) => {
-        hits = hits.concat(batch);
-      },
-    })
-    .then(() => console.log(hits));
-
-  const objects = [
-    {
-      UpdatedDate: "2021-08-15T15:37:37.691Z",
-      title: "青でいいらしい",
-      subTitle: "〜日記〜",
-      main: "main",
-      images: [],
-      movies: [],
-      id: 193,
-    },
-  ];
-
-  // algoliaに書き込む
-  // index.saveObjects(objects, { autoGenerateObjectIDIfNotExist: true }).then(({ objectIDs }) => {
-  //   console.log(objectIDs);
-  // });
 
   res
     .status(200)
