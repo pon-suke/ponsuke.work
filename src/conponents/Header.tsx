7e6ecdae0dd28b370a1ade1d5a8159839ac164d7
HoorayTritium <48468687+HoorayTritium@users.noreply.github.com>
2022-12-30 19:40:03 +0900

    version管理

--- src/conponents/Header.tsx
+++ src/conponents/Header.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 import Link from "next/link";
-import AdSense from "react-adsense";
+import {Adsense} from "@ctrl/react-adsense";
 import Script from "next/script";
 import SearchDialog from "./SearchDialog";
+import Head from "next/head";
 
 const Header: React.FC = () => (
   <>
-    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></Script>
-    <AdSense.Google client="ca-pub-4852357139979343" slot="1601568202" />
+    <Head children={""}>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    </Head>
+    <Adsense client="ca-pub-4852357139979343" slot="1601568202" />
 
     <SearchDialog />
     <Link href="/p/1">
